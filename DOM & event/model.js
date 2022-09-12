8cf979f975985aaea5025946874150f885a1bd5d
NaveenDev <103094463+navee123n@users.noreply.github.com>
2022-09-11 21:11:23 -0700

    Update model.js

--- DOM & event/model.js
+++ DOM & event/model.js
@@ -1,5 +1,29 @@
 'use strict';
-const modal=document.querySelectorAll('.show-modal');
-console.log(modal);
-for(let i=0;i<modal.length;i++)
-    console.log(modal[i].textContent);
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const btnCloseModal=document.querySelector('.close-modal');
+const btnOpenModal=document.querySelectorAll('.show-modal');
+console.log(btnOpenModal);
+//class manipulation;
+
+const openModal=function(){
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+const closeModal=function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');  
+};
+
+for(let i=0; i< btnOpenModal.length; i++)
+btnOpenModal[i].addEventListener('click',openModal);
+btnCloseModal.addEventListener('click',closeModal);
+overlay.addEventListener('click',closeModal); 
+
+document.addEventListener('keydown',function(f){
+    console.log(f.key);
+    if(f.key==='Escape' && !modal.classList.contains('hidden')){
+     closeModal();
+}
+});
